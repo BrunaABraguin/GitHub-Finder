4d19568245e6a206c3aed61c67f1f67ac2b91a84
Bruna A Braguin <andradevbruna@gmail.com>
2021-07-08 02:59:41 -0300

    Link react-router-dom

--- src/pages/Repositories/styled.js
+++ src/pages/Repositories/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Title = styled.h1`
   text-align: center;
@@ -23,4 +24,14 @@ export const Container = styled.div`
   width: 100%;
   max-width: 990px;
   margin: 0 auto;
+`;
+
+export const LinkHome = styled(Link)`
+  display: block;
+  width: 4rem;
+  text-align: center;
+  margin: 2rem auto;
+  background-color: #000;
+  color: #fff;
+  text-decoration: none;
 `;
