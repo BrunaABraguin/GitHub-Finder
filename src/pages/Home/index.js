77337e4b8f137a5e59d3b3387b42da13f64ed018
Bruna A Braguin <andradevbruna@gmail.com>
2021-07-08 03:18:24 -0300

    Conditional rendering

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,35 +6,45 @@ import { useHistory } from 'react-router-dom';
 export default function Home(props) {
   const history = useHistory();
   const [user, setUser] = useState('');
+  const [error, setError] = useState(false);
 
   function handleSearch() {
-    axios.get(`https://api.github.com/users/${user}/repos`).then((response) => {
-      const repositories = response.data;
-      const repositoriesName = [];
+    axios
+      .get(`https://api.github.com/users/${user}/repos`)
+      .then((response) => {
+        const repositories = response.data;
+        const repositoriesName = [];
 
-      repositories.map((repository) => {
-        repositoriesName.push(repository.name);
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name);
+        });
+
+        setError(false);
+        localStorage.setItem(
+          'repositoriesName',
+          JSON.stringify(repositoriesName)
+        );
+
+        history.push('/repositories');
+      })
+      .catch((err) => {
+        setError(true);
       });
-
-      localStorage.setItem(
-        'repositoriesName',
-        JSON.stringify(repositoriesName)
-      );
-
-      history.push('/repositories');
-    });
   }
   return (
-    <S.Container>
-      <S.Input
-        className='userInput'
-        placeholder='Usuário'
-        value={user}
-        onChange={(e) => setUser(e.target.value)}
-      />
-      <S.Button type='button' onClick={handleSearch}>
-        Pesquisar
-      </S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Content>
+        <S.Input
+          className='userInput'
+          placeholder='Usuário'
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+        />
+        <S.Button type='button' onClick={handleSearch}>
+          Pesquisar
+        </S.Button>
+      </S.Content>
+      {error ? <S.ErrorMsg>Ocorreu um erro, tente novamente</S.ErrorMsg> : ''}
+    </S.HomeContainer>
   );
 }
